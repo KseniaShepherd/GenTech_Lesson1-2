eec4d586a4e52f72fe59150bc37279b0ecb0f348
KShepherd <43641658+KseniaShepherd@users.noreply.github.com>
2023-05-16 00:16:39 +0300

    categoryContainers

--- index.jsx
+++ index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {getCategories} from '../../requests/getCategories'
+import Category from '../Category';
 import s from './index.module.css'
 
-export default function ProductsItem({ id, title, image, price }) {
-    return (
-        <Link to={`/product/${id}`}>
-            <div className={s.productsItem}>
-                <p>{title}</p>
-                <img src={image} alt={title} />
-                <p>{price} $</p>
-            </div>
-        </Link>
+export default function CategoriesContainer() {
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        getCategories(setCategories)
+    },[]);
 
-    )
+  return (
+    <div className={s.categoriesContainer}>
+        {categories.map(el=> <Category key={Date.now()} el={el}/> )}
+    </div>
+  )
 }
